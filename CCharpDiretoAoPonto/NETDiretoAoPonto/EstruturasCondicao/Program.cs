4ec39052ecd4b8285627b109f068cfe653458c4d
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-03-17 20:55:20 -0300

    CSharpDiretoAoPonto: Estruturas De Controle e Fluxo

---
+++ CCharpDiretoAoPonto/NETDiretoAoPonto/EstruturasCondicao/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace EstruturasCondicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var notaDigitada = Console.ReadLine();
+
+            //var nota = int.Parse(notaDigitada);
+
+            //if(nota >= 70)
+            //{
+            //    Console.WriteLine("Aprovado");
+            //} else if (nota >= 40)
+            //{
+            //    Console.WriteLine("Em recuperação");
+            //} else
+            //{
+            //    Console.WriteLine("Reprovado");
+            //}
+
+            // Switch - case
+
+            Console.WriteLine("Seja bem vindo a empresa x");
+
+            Console.WriteLine("Digite 1- Contratação de um plano");
+            Console.WriteLine("Digite 2- Reclamação de serviço prestado");
+            Console.WriteLine("Digite 3- Segunda via de boleto");
+            Console.WriteLine("Digite 4- Sair");
+
+            var opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("Informações do plano novo");
+                    break;
+                case "2":
+                    Console.WriteLine("Fale sobre sua reclamação");
+                    break;
+                case "3":
+                    Console.WriteLine("Segunda via de boleto enviada por e-mail");
+                    break;
+                case "4":
+                    Console.WriteLine("Tenha um bom dia!");
+                    break;
+                default:
+                    Console.WriteLine("Opção não encontrada.");
+                    break;
+            }
+        }
+    }
+}
---
+++ CCharpDiretoAoPonto/NETDiretoAoPonto/EstruturasRepeticao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EstruturasRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // For
+            Console.WriteLine("Digite uma sequência de números separados por espaço.");
+
+            var numerosTexto = Console.ReadLine();
+
+            var numeros = numerosTexto.Split(' ');
+
+            Console.WriteLine("Números: ");
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+            Console.WriteLine("Usando While: ");
+
+            // While
+            var contador = 0;
+
+            while (contador < numeros.Length)
+            {
+                Console.WriteLine(numeros[contador]);
+                contador++;
+            }
+
+            //ForEach
+            Console.WriteLine("Usando foreach");
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
