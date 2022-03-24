329a0fcd3221f183da309fccd4358ca292b51a97
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-03-24 08:31:06 -0300

    Fundamentos C#: Trabalhando com Arquivos e Diretorios

---
+++ CCharpDiretoAoPonto/NETDiretoAoPonto/Diretorios/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Diretorios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Directory e DirectoryInfo
+
+            //var folderName = "pasta";
+            //var subFolderName = "subpasta";
+            //var subFolderNameStatic = "subpasta_usingstatic";
+
+            //var directoyInfoSubFolder = new DirectoryInfo(subFolderName);
+
+            //if(!Directory.Exists(subFolderName) || !directoyInfoSubFolder.Exists)
+            //{
+            //    Directory.CreateDirectory(subFolderNameStatic);
+            //    Directory.CreateDirectory(folderName);
+            //    directoyInfoSubFolder.Create();
+
+            //    directoyInfoSubFolder.MoveTo($"{folderName}//{subFolderName}");
+            //    Directory.Move(subFolderNameStatic, @$"{folderName}\{subFolderNameStatic}");
+            //}
+
+            //var name = directoyInfoSubFolder.Name;
+            //var parent = directoyInfoSubFolder.Parent;
+            //var root = directoyInfoSubFolder.Root;
+            //var exists = directoyInfoSubFolder.Exists;
+
+            //foreach (var directory in Directory.GetDirectories(folderName))
+            //{
+            //    Console.WriteLine(directory);
+            //}
+
+            //Directory.Delete($@"{folderName}\{subFolderName}");
+
+            #endregion
+
+            #region File e FileInfo
+
+            var file = @"pasta\texto.txt";
+
+            if(!File.Exists(file))
+            {
+                File.CreateText(file);
+            }
+
+            var fileInfo = new FileInfo(file);
+
+            Console.WriteLine($"Nome:{fileInfo.Name}, Tamanho: {fileInfo.Length}, Data de atualização: {fileInfo.LastWriteTime}");
+
+            #endregion
+        }
+    }
+}
