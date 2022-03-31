a6423415e5c91cf6517e63efe259090c87f46ad0
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-03-31 15:56:19 -0300

    ASP.NET Core: Arquitetura Limpa -> Camada Core

---
+++ FormacaoApNetCore/DevFreela/DevFreela.Core/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullName, string email, DateTime birthDate)
+        {
+            FullName = fullName;
+            Email = email;
+            BirthDate = birthDate;
+            Active = true;
+            CreatedAt = DateTime.Now;
+
+            Skills = new List<UserSkill>();
+            OwnedProjects = new List<Project>();
+            FreelanceProjects = new List<Project>();
+        }
+
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool Active { get; private set; }
+
+        public List<UserSkill> Skills { get;private set; }
+        public List<Project> OwnedProjects { get; private set; }
+        public List<Project> FreelanceProjects { get; private set; }
+    }
+}
